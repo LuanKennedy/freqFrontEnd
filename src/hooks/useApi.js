3b41e299698048d92aaf9db3598eb35b34cd7b58
LuanKennedy <luanc7459@gmail.com>
2023-05-13 16:52:32 -0300

    routes change

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 const useApi = () => {
   async function useGet(url) {
-    const response = await axios.get("http://localhost:8080/api" + url);
+    const response = await axios.get("https://git.heroku.com/frontfreq.git" + url);
     return response;
   }
 
   async function useDelete(url) {
-    const response = await axios.delete("http://localhost:8080/api" + url);
+    const response = await axios.delete("https://git.heroku.com/frontfreq.git" + url);
     return response;
   }
 
   async function usePost(url, data) {
-    const response = await axios.post("http://localhost:8080/api" + url, data);
+    const response = await axios.post("https://git.heroku.com/frontfreq.git    " + url, data);
     return response;
   }
 
   async function usePut(url, data) {
-    const response = await axios.put("http://localhost:8080/api" + url, data);
+    const response = await axios.put("https://git.heroku.com/frontfreq.git    " + url, data);
     return response;
   }
 
